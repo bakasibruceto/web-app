bf4a02a94ac5b017e7f4e05c39d94be2d91c152b
bakasibruceto <abla09027@gmail.com>
2023-10-02 21:05:08 +0800

    remove sass

--- README.md
+++ README.md
@@ -3,11 +3,7 @@ create `.env` file and refer to `.env.example`
 
 ## Dependency
 - Composer
-- Node.js
-
 ## dotenvphp
 `composer require vlucas/phpdotenv`
 
-## SASS
-`npm install --save-dev sass`
 
--- src/view/login.php
+++ src/view/login.php
@@ -5,9 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    
 </head>
 
 <body>
+    <h1 class = "ha">Hello</h1>
     <form action="POST">
         <label for="username">username</label>
         <input type="text" name="username">
