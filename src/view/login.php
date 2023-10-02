7bcfc2335f30cf78cb0c85c3faedf956c0ac97ff
bakasibruceto <abla09027@gmail.com>
2023-10-02 20:47:31 +0800

    sass

---
+++ src/view/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="POST">
+        <label for="username">username</label>
+        <input type="text" name="username">
+        <label for="password">password</label>
+        <input type="password" name="password">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
