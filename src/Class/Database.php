a8aeba8c8fddd1ce83e8e4a6bcd98016630e5fe8
bakasibruceto <abla09027@gmail.com>
2023-10-02 17:20:46 +0800

    add Database class
    - Database Connections
    - Create Tables

---
+++ src/Class/Database.php
@@ -0,0 +1,33 @@
+<?php
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+
+class Database
+{
+    private $conn;
+
+    public function __construct($host, $dbname, $user, $password)
+    {
+        try {
+            $this->conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connection Successful";
+        } catch (PDOException $e) {
+            echo "Connection Failed: " . $e->getMessage();
+        }
+    }
+
+    public function createTable($tableName, $sql)
+    {
+        try {
+            $this->conn->exec($sql);
+            echo "$tableName Table Created Successfully";
+        } catch (PDOException $e) {
+            echo "Error creating $tableName table: " . $e->getMessage();
+        }
+    }
+}
+
+
