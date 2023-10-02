c55dbc8f99a5336f83a1a4868dc5a0dcf1c0d7f6
bakasibruceto <abla09027@gmail.com>
2023-10-02 18:06:34 +0800

    file formatting

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ require __DIR__ . "/vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
 require_once "src/class/database.class.php";
-require_once "src/config/tables.config.php";
+require_once "src/config/table.config.php";
