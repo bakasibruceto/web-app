52855664f39ec9f9666a73d35103e2a6410fbb7d
bakasibruceto <abla09027@gmail.com>
2023-10-02 17:21:37 +0800

    convert to OOP

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+require __DIR__ . "/vendor/autoload.php";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
-require_once "conn.php";
+require_once "src/Class/Database.php";
+require_once "src/config/Tables.php";
 
 
 
