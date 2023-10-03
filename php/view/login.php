0cdc6551ffb17f678fae94a593b39d2b0d9a555b
bakasibruceto <abla09027@gmail.com>
2023-10-03 23:41:12 +0800

    Create login.php

---
+++ php/view/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    
+</head>
+
+<body>
+    <h1 class = "ha">Hello</h1>
+    <form action="POST">
+        <label for="username">username</label>
+        <input type="text" name="username">
+        <label for="password">password</label>
+        <input type="password" name="password">
+        <input type="submit" name="login">
+    </form>
+</body>
+
+</html>
