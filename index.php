e2f6b11bdb0dabb88e167159cc2f6979865ca0c1
bakasibruceto <abla09027@gmail.com>
2023-10-03 23:39:38 +0800

    modified file paths

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 require __DIR__ . "/vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
-require_once "src/class/database.class.php";
-require_once "src/config/table.config.php";
+require "php/class/DBHandle.class.php";
+require_once "php/class/createTable.class.php";
+require_once "php/config/table.config.php";
