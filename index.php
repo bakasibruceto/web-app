7c154c805b7dc51ba0414043f97eeab6638bd8c4
bakasibruceto <abla09027@gmail.com>
2023-10-05 04:18:22 +0800

    signup.php header

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require __DIR__ . "/vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
-require "php/class/DBHandle.class.php";
-require_once "php/class/createTable.class.php";
+require "php/class/Database.class.php";
+require_once "php/class/Tables.class.php";
 require_once "php/config/table.config.php";
+
+header ("location: php/view/Signup.php");
