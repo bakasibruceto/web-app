5eadc4742139accf2fd1aef6bdc30a9f4ff0fb71
bakasibruceto <abla09027@gmail.com>
2023-10-06 00:43:27 +0800

    fixed .env file path

--- php/include/Signup.inc.php
+++ php/include/Signup.inc.php
@@ -1,21 +1,20 @@
 <?php
-
-
+require '../../vendor/autoload.php';
 if(isset($_POST['submit'])){
     
     // Grab data
     $username = $_POST["username"];
     $email = $_POST["email"];
     $password = $_POST["password"];
-    // $repassword = $_POST["repassword"];
+    $repassword = $_POST["repassword"];
 
 
     require_once "../class/Database.class.php";
     require_once "../class/Signup.class.php";
     require_once "../class/SignupController.class.php";
 
-    $signup = new Signup($username, $email, $password);
-    $signup->setUser($username, $email, $password);
+    $signup = new SignupController($username, $email, $password, $repassword);
+    $signup->signupUser();
 
     // Back to Frontpage
     header("location: ../../index.php");
