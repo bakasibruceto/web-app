810ddbe7825c290ba83860a6c7d6422b9a8abb05
bakasibruceto <abla09027@gmail.com>
2023-10-06 14:04:43 +0800

    pico css

--- php/view/login.php
+++ php/view/login.php
@@ -5,19 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href=" https://cdn.jsdelivr.net/npm/@picocss/pico@1.5.10/css/pico.min.css" rel="stylesheet">
 
-    
 </head>
 
 <body>
-    <h1 class = "ha">Hello</h1>
-    <form action="POST">
-        <label for="username">username</label>
-        <input type="text" name="username">
-        <label for="password">password</label>
-        <input type="password" name="password">
-        <input type="submit" name="login">
-    </form>
+    <main class="container">
+        <div class="grid">
+            <h1>Login</h1>
+            <form action="../include/login.inc.php" method="POST">
+                <label for="username">username or email
+                    <input type="text" name="username">
+                </label>
+                <label for="password">password
+                    <input type="password" name="password">
+                </label>
+                <button type="submit" name="submit">Login</button>
+            </form>
+        </div>
+    </main>
+
 </body>
 
 </html>
