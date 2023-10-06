7bbb69d69f2b4d67de6deaa5760b038c8647f859
bakasibruceto <abla09027@gmail.com>
2023-10-06 14:04:56 +0800

    Update Login.inc.php

---
+++ php/include/Login.inc.php
@@ -0,0 +1,24 @@
+<?php
+require '../../vendor/autoload.php';
+
+
+if(isset($_POST['submit'])){
+    
+    // Grab data
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    require_once "../class/Database.class.php";
+    require_once "../class/Login.class.php";
+    require_once "../class/LoginController.class.php";
+    $login = new LoginController($username,$password);
+    
+    $login->loginUser();
+
+    // Back to Frontpage
+    header("location: ../view/user/user.php");
+
+
+}
+
+?>
