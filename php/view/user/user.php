1202d181f6181e5c95e5b21aeb9cca84f5d32690
bakasibruceto <abla09027@gmail.com>
2023-10-07 14:09:38 +0800

    Update user.php

--- php/view/user/user.php
+++ php/view/user/user.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=\, initial-scale=1.0">
     <title>user</title>
+    <link href=" https://cdn.jsdelivr.net/npm/@picocss/pico@1.5.10/css/pico.min.css" rel="stylesheet">
 </head>
+</head>
+
 <body>
-    <h1>USERPAGE</h1>
+    <main class="container">
+        <h1>USERPAGE</h1>
+        <button type="submit" name="submit">sign out</button>
+    </main>
+
 </body>
+
 </html>
