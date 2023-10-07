ed1514a295422cf90c36c2a1c0f2b8ce1237d3bf
bakasibruceto <abla09027@gmail.com>
2023-10-07 21:30:19 +0800

    user roles added

--- php/class/Login.class.php
+++ php/class/Login.class.php
@@ -3,30 +3,44 @@ class Login extends Database
 {
     public function getUser($username, $password): void
     {
-        $query = "SELECT id, username, password FROM user WHERE username = :username OR email = :email;";
-        $stmt = $this->connect()->prepare($query);
-        $stmt->bindParam(":username", $username);
-        $stmt->bindParam(":email", $username);
-        try {
-            $stmt->execute();
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        $accountTypes = ["superadmin", "admin", "user"];
+        $user = null;
+        $accountType = null;
 
-            if (!$user) {
-                header("location: ../view/login.php?error=usernotfound");
+        foreach ($accountTypes as $role) {
+            $query = "SELECT id, username, password FROM account WHERE (username = :username OR email = :email) AND role = :role LIMIT 1;";
+            $stmt = $this->connect()->prepare($query);
+            $stmt->bindParam(":username", $username);
+            $stmt->bindParam(":email", $username);
+            $stmt->bindParam(":role", $role);
+
+            try {
+                $stmt->execute();
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($user) {
+                    $accountType = $role;
+                    break; // Exit the loop if a user is found
+                }
+            } catch (PDOException $e) {
+                header("location: ../view/login.php?error=stmtfailed");
                 exit();
             }
-            // check password
-            if (!password_verify($password, $user["password"])) {
-                header("location: ../view/login.php?error=wrongpassword");
-                exit();
-            }
+        }
 
-            session_start();
-            $_SESSION["id"] = $user["id"];
-            $_SESSION["username"] = $user["username"];
-        } catch (PDOException $e) {
-            header("location: ../view/login.php?error=stmtfailed");
+        if (!$user) {
+            header("location: ../view/login.php?error=usernotfound");
             exit();
         }
+
+        if (!password_verify($password, $user["password"])) {
+            header("location: ../view/login.php?error=wrongpassword");
+            exit();
+        }
+
+        session_start();
+        $_SESSION["id"] = $user["id"];
+        $_SESSION["username"] = $user["username"];
+        header("location: ../view/$accountType/$accountType.php");
     }
 }
