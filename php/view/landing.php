21c2da66d880c845db841d4b92cc532bf821782a
bakasibruceto <abla09027@gmail.com>
2023-10-07 21:34:18 +0800

    Update landing.php

---
+++ php/view/landing.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link href=" https://cdn.jsdelivr.net/npm/@picocss/pico@1.5.10/css/pico.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <main class="container">
+        <div class="grid">
+            <h1>Landing Page</h1>
+            <div>
+                <a href="Login.php"><button>Login</button></a>
+                <a href="Signup.php"><button>Signup</button></a>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
