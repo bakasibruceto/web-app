4d421c43fc1634b129c62b7d86c274e7224303f2
bakasibruceto <abla09027@gmail.com>
2023-10-07 21:27:53 +0800

    landing page added

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 require __DIR__ . "/vendor/autoload.php";
 Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
+
 require "php/class/Database.class.php";
-require_once "php/class/Tables.class.php";
+require_once "php/class/Query.class.php";
 require_once "php/config/table.config.php";
 
-header ("location: php/view/Signup.php");
+header ("location: php/view/landing.php");
