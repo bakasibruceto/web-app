7eb6bca239ce13ef957fbbffa2a9e1580bbb31b0
bakasibruceto <abla09027@gmail.com>
2023-10-07 21:44:21 +0800

    modified queries

--- php/config/table.config.php
+++ php/config/table.config.php
@@ -1,36 +1,42 @@
 <?php
-$sqlUserTable = "CREATE TABLE IF NOT EXISTS user (
+
+$sqlAccountsTable = "CREATE TABLE IF NOT EXISTS account (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
     email VARCHAR(255),
     password VARCHAR(255),
+    role VARCHAR(225) DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )";
 
 $sqlLogsTable = "CREATE TABLE IF NOT EXISTS log (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id INT, 
     user_action VARCHAR(225),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (account_id) REFERENCES account(id) 
+)";
+
+$sqlUserTable = "CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id INT, 
+    FOREIGN KEY (account_id) REFERENCES account(id) 
 )";
 
 $sqlAdminTable = "CREATE TABLE IF NOT EXISTS admin (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    account_id INT, 
+    FOREIGN KEY (account_id) REFERENCES account(id)
 )";
 
 $sqlSuperAdminTable = "CREATE TABLE IF NOT EXISTS superAdmin (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    account_id INT,
+    FOREIGN KEY (account_id) REFERENCES account(id)
 )";
 
-$sql = [$sqlUserTable, $sqlAdminTable, $sqlLogsTable, $sqlSuperAdminTable];
+$sql = [$sqlAccountsTable, $sqlLogsTable, $sqlAdminTable, $sqlSuperAdminTable, $sqlUserTable ];
 
-$database = new Tables();
+$database = new Query();
 
-$database->createTable($sql);
+$database->createQuery($sql);
