0fc625bc5a20a7f67b7c95367206787b70963b09
bakasibruceto <abla09027@gmail.com>
2023-10-07 21:34:14 +0800

    Update admin.php

--- php/view/admin/admin.php
+++ php/view/admin/admin.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=\, initial-scale=1.0">
     <title>admin</title>
+    <link href=" https://cdn.jsdelivr.net/npm/@picocss/pico@1.5.10/css/pico.min.css" rel="stylesheet">
 </head>
+</head>
+
 <body>
-    <h1>ADMINPAGE</h1>
+    <main class="container">
+        <h1>ADMIN PAGE</h1>
+        <button type="submit" name="submit">sign out</button>
+    </main>
+
 </body>
+
 </html>
