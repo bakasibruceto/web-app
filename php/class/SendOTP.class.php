3c81f1cfbadd9e9fce3e3d021f4832e14d7140c1
bakasibruceto <abla09027@gmail.com>
2023-10-08 21:24:37 +0800

    Create SendOTP.class.php

---
+++ php/class/SendOTP.class.php
@@ -0,0 +1,59 @@
+<?
+Dotenv\Dotenv::createImmutable(__DIR__ . '../../..')->load();
+require '../../../vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../../../vendor/phpmailer/phpmailer/src/Exception.php';
+require '../../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../../../vendor/phpmailer/phpmailer/src/SMTP.php';
+
+
+
+class SendOtpChangePassword extends ForgotPasswordController
+{
+    private $SmtpEmail;
+    private $SmtpPassword;
+    public function SendOtp($username, $email)
+    {
+        $mail = new PHPMailer(true);
+
+        $this->SmtpEmail=$_ENV['SMTP_EMAIL'];
+        $this->SmtpPassword=$_ENV['SMTP_PASSWORD'];
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+            $mail->isSMTP(); //Send using SMTP
+            $mail->Host = 'smtp.example.com'; //Set the SMTP server to send through
+            $mail->SMTPAuth = true; //Enable SMTP authentication
+            $mail->Username = $this->SmtpEmail; 
+            $mail->Password = $this->SmtpPassword; 
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+            $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('from@example.com', 'Mailer');
+            $mail->addAddress($email, $username); //Add a recipient
+            $mail->addReplyTo('info@example.com', 'Information');
+            $mail->addCC('cc@example.com');
+            $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            $mail->addAttachment('/var/tmp/file.tar.gz'); //Add attachments
+            $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //Optional name
+
+            //Content
+            $mail->isHTML(true); //Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+}
+
