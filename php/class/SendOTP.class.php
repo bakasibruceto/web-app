9fd5fdf9b3afb21d5575beecc6baa7e0c7bf7a0f
bakasibruceto <abla09027@gmail.com>
2023-10-09 14:55:49 +0800

    Update SendOTP.class.php

--- php/class/SendOTP.class.php
+++ php/class/SendOTP.class.php
@@ -1,14 +1,15 @@
 <?
 Dotenv\Dotenv::createImmutable(__DIR__ . '../../..')->load();
+
 require '../../../vendor/autoload.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require '../../../vendor/phpmailer/phpmailer/src/Exception.php';
 require '../../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
 require '../../../vendor/phpmailer/phpmailer/src/SMTP.php';
-
-
 
 class SendOtpChangePassword extends ForgotPasswordController
 {
